56bbae3589dc909a52003a6c29b93c2067c082f8
Joshua Nyambane <nyamsjosh@gmail.com>
2024-07-12 10:44:18 +0300

    innitial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector('main')
+main.id = 'mainContainer'
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'Joshua is the champion.'
